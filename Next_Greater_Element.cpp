ab04c801186dda1ba373b581e593dfae4856872b
isayakmondal <isayakmondal@gmail.com>
2022-01-14 12:42:43 +0530

    ++

---
+++ Next_Greater_Element.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// O(n^2) sol using vectors
+
+vector<int> NGE(vector<int> v){
+
+    int n = v.size();
+    vector<int> nge(n, -1);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (v[j] > v[i])
+            {
+                nge[i] = v[j];
+                break;
+            }
+            else
+            {
+                nge[i] = -1;
+            }
+        }
+    }
+
+    return nge;
+
+
+}
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cin >> v[i];
+    }
+
+    vector<int> nge = NGE(v);
+
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " " << nge[i] << endl;
+    }
+
+    return 0;
+}
