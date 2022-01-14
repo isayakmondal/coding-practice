c3f1a538a47b0f8494751518c51783e225ac1c59
isayakmondal <isayakmondal@gmail.com>
2022-01-14 16:38:16 +0530

    Add O(n) solution

--- Next_Greater_Element.cpp
+++ Next_Greater_Element.cpp
@@ -31,6 +31,31 @@ vector<int> NGE(vector<int> v){
 
 }
 
+// O(n) sol using stack
+
+vector<int> NGE_stack(vector<int> v){
+
+    int n = v.size();
+    vector<int> nge(n, -1);
+    stack<int> st;
+    int ct=0;
+
+    for (int i = 0; i < n; i++)
+    {
+        while(!st.empty() && v[i]>v[st.top()]){
+
+            int temp = st.top();
+            nge[temp]=i;
+            st.pop();
+            ct++;
+        }
+        st.push(i);
+    }
+    // cout<<ct<<endl;
+    return nge;
+
+
+}
 
 
 
@@ -46,12 +71,13 @@ int main(int argc, char const *argv[])
         cin >> v[i];
     }
 
-    vector<int> nge = NGE(v);
+    // vector<int> nge = NGE(v);
+    vector<int> nge = NGE_stack(v);
 
     
     for (int i = 0; i < n; i++)
     {
-        cout << v[i] << " " << nge[i] << endl;
+        cout << v[i] << " " << (nge[i]==-1?-1:v[nge[i]]) << endl;
     }
 
     return 0;
