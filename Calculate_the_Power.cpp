fd26ecb5db6f720ed2081fabaa82051681a364a3
isayakmondal <isayakmondal@gmail.com>
2022-02-26 18:33:22 +0530

    Added: super_pow.cpp  Modified: Calculate_the_Power.cpp

--- Calculate_the_Power.cpp
+++ Calculate_the_Power.cpp
@@ -4,19 +4,42 @@ using namespace std;
 
 #define ll long long
 
-const ll M = 1e9 +7;
+const ll M = 1e9 + 7;
 
-ll calc_power(int a, int b){
+// Recursion using binary exponentiation
+ll calc_power_rec(int a, int b)
+{
 
-    if(b==0) return 1;
-    ll res = calc_power(a,b/2);
-    
-    if(b%2==0){
-        return (res*res)%M;
+    if (b == 0)
+        return 1;
+    ll res = calc_power_rec(a, b / 2);
+
+    if (b % 2 == 0)
+    {
+        return (res * res) % M;
     }
-    else{
-        return (a * calc_power(a,b-1))%M;
+    else
+    {
+        return (a * calc_power_rec(a, b - 1)) % M;
     }
+}
+
+// Iterative using binary exponentiation
+ll calc_power_iter(ll a, ll b)
+{
+
+    ll ans = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            ans = (ans * a) % M;
+        }
+        a *= a % M;
+        b >>= 1;
+    }
+
+    return ans;
 }
 
 int main()
@@ -29,10 +52,9 @@ int main()
     // cin >> t;
     while (t--)
     {
-        int a,b;
-        cin>>a>>b;
-        cout<<calc_power(a,b)%M<<endl;
-
+        ll a, b;
+        cin >> a >> b;
+        cout << calc_power_iter(a, b) << endl;
     }
 
     return 0;
---
+++ super_pow.cpp
@@ -0,0 +1,38 @@
+//Using ETF and Euler's Theorem
+class Solution
+{
+public:
+    const long long M = 1337;
+    const long long m = 1140;
+
+    long long calc_power_iter(long long a, long long b)
+    {
+
+        long long ans = 1;
+        a = a % M;
+        while (b > 0)
+        {
+            if (b & 1)
+            {
+                ans = (ans * a) % M;
+            }
+            a = (a * a) % M;
+            b >>= 1;
+        }
+
+        return ans;
+    }
+
+    int superPow(int a, vector<int> &v)
+    {
+
+        long long num = 0;
+
+        for (int val : v)
+        {
+            num = (num * 10 + val) % m;
+        }
+
+        return calc_power_iter((long long)a, num);
+    }
+};
