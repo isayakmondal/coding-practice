c81d4c0f3ee6e82ac3db64b1d5fbbff5c942c3b9
isayakmondal <isayakmondal@gmail.com>
2022-03-03 12:58:09 +0530

    Modified: Monk_and_Divisor_Conundrum.cpp

--- Monk_and_Divisor_Conundrum.cpp
+++ Monk_and_Divisor_Conundrum.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 #define ll long long
 
-const int N =  2 * 1e5 +10;
+const int N = 2 * 1e5 + 10;
 int hsh[N];
 ll multiple_ct[N];
 
@@ -14,39 +14,35 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    
-
     int n;
-    cin>>n;
+    cin >> n;
     for (int i = 0; i < n; i++)
     {
         int x;
-        cin>>x;
+        cin >> x;
         hsh[x]++;
-
     }
 
-      for (int i = 1; i < N; i++)
+    for (int i = 1; i < N; i++)
     {
-        for (int j = i; j < N; j+=i)
+        for (int j = i; j < N; j += i)
         {
-            multiple_ct[i]+=hsh[j];
+            multiple_ct[i] += hsh[j];
         }
-        
     }
-    
 
     ll t = 1;
     cin >> t;
     while (t--)
     {
-        int p,q;
-        cin>>p>>q;
-        ll lcm = (p * 1LL * q)/__gcd(p,q);
+        int p, q;
+        cin >> p >> q;
+        ll lcm = (p * 1LL * q) / __gcd(p, q);
         ll ans = multiple_ct[p] + multiple_ct[q];
-        if(lcm<N) ans = ans - multiple_ct[lcm];
-       
-        cout<<ans<<endl;
+        if (lcm < N)
+            ans = ans - multiple_ct[lcm];
+
+        cout << ans << endl;
     }
 
     return 0;
