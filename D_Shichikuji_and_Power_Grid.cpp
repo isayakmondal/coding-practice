5dbdbb1a016184df827b4a197351a2c11f32285e
isayakmondal <isayakmondal@gmail.com>
2022-04-23 18:14:42 +0530

    Added: D_Shichikuji_and_Power_Grid.cpp

---
+++ D_Shichikuji_and_Power_Grid.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        
+
+    }
+
+    return 0;
+}
