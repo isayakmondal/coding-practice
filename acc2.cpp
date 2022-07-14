bb4a219eb9a48be2f179a2c764134d1fc81533c9
isayakmondal <isayakmondal@gmail.com>
2022-07-14 11:54:07 +0530

    Modified: acc2.cpp

--- acc2.cpp
+++ acc2.cpp
@@ -1,26 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
-int OperationsBinaryString(char* str)
+
+// Using only the pointer to char.
+int OperationsBinaryString(char *str)
 {
     int size = strlen(str);
 
-    if(size == 0) return -1;
+    if (size == 0)
+        return -1;
 
-    int dig1 = str[0] - '0';
-    
+    int dig1 = *str - '0';
+    str++;
 
     int ans = dig1;
-    int i = 1;
-        while(i<size)
-    {
-        
-        int temp = i;
-        i++;
-        int dig2 = str[i] - '0';
-        
 
-        switch (str[temp])
+    while (*str != '\0')
+    {
+        char temp = *str++;
+        int dig2 = *str - '0';
+
+        switch (temp)
         {
         case 'A':
             ans &= dig2;
@@ -31,20 +30,56 @@ int OperationsBinaryString(char* str)
         case 'C':
             ans ^= dig2;
             break;
-        
+
         default:
             break;
         }
-        i++;
     }
     return ans;
 }
- 
+
+// int OperationsBinaryString(char* str)
+// {
+//     int size = strlen(str);
+
+//     if(size == 0) return -1;
+
+//     int dig1 = str[0] - '0';
+
+//     int ans = dig1;
+//     int i = 1;
+//         while(i<size)
+//     {
+
+//         int temp = i;
+//         i++;
+//         int dig2 = str[i] - '0';
+
+//         switch (str[temp])
+//         {
+//         case 'A':
+//             ans &= dig2;
+//             break;
+//         case 'B':
+//             ans |= dig2;
+//             break;
+//         case 'C':
+//             ans ^= dig2;
+//             break;
+
+//         default:
+//             break;
+//         }
+//         i++;
+//     }
+//     return ans;
+// }
+
 int main()
 {
     string s;
-    getline(cin,s);
-    int len=s.size();
-    char *str=&s[0];
-    cout<<OperationsBinaryString(str);
+    getline(cin, s);
+    int len = s.size();
+    char *str = &s[0];
+    cout << OperationsBinaryString(str);
 }
